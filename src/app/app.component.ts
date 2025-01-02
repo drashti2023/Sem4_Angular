a27e1c92dfb4de6a4a0d7c35ab188af9a9792479
drashti2023 <drashtirathod77@gmail.com>
2025-01-02 20:45:12 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgFor,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CRUD_array';
+  tempValue='';
+  arr=["Drashti","Rajvi","Vritti"];
+  btnValue="Add";
+  isEditable=-1;
+
+  addStudent(e:any){
+  
+    if(this.isEditable == -1 && this.tempValue!=''){
+      this.arr.push(this.tempValue);
+      this.tempValue='';
+      this.btnValue="Add";
+      return
+    }
+    this.arr[this.isEditable]=this.tempValue;
+    this.isEditable=-1;
+    this.tempValue='';
+  
+  }
+  deleteValue(i:any){
+    this.arr.splice(i,1);
+  }
+  
+  editValue(i:any){
+    this.isEditable=i;
+    this.btnValue="Edit";
+    this.tempValue=this.arr[i];
+
+  }
+  
 }
